556310acb718de32f54d64ab14a0a027618e708c
aumetov7 <aumetov7@gmail.com>
2022-08-16 18:15:12 +0600

    Detail Location View Controller

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -34,6 +34,18 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         updateLabels()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     @IBAction func getLocation() {
         let authStatus = locationManager.authorizationStatus
         
@@ -60,6 +72,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         }
         
         updateLabels()
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
     }
     
     // MARK: - CLLocationManagerDelegate
